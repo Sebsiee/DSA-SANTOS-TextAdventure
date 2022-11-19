d7801fd116dec4025cb09c5eddcc780af41ba5c3
unknown <santosbasty4@gmail.com>
2022-11-19 14:15:36 +0800

    Third Commit - Story Progress (1/3)

--- TextAdventure.py
+++ TextAdventure.py
@@ -1,16 +1,21 @@
 import time, sys
 
+dict = {}
+info = []
+backpack = {}
+tower = {}
+
 def typingPrint(text):
   for character in text:
     sys.stdout.write(character)
     sys.stdout.flush()
-    time.sleep(0.02)
+    time.sleep(0)
 
 def typingInput(text):
   for character in text:
     sys.stdout.write(character)
     sys.stdout.flush()
-    time.sleep(0.02)
+    time.sleep(0)
   value = input()  
   return value 
 
@@ -41,13 +46,13 @@ def welcome():
 #Player Customization
 def intro():
     print(".")
-    time.sleep(1)
+    time.sleep(0)
     print(".")
-    time.sleep(1)
+    time.sleep(0)
     print(".")
-    time.sleep(1)
+    time.sleep(0)
     print(".")
-    time.sleep(1)
+    time.sleep(0)
     typingPrint("""
 
         It was dark....
@@ -77,18 +82,131 @@ def intro():
     
     while True:
         if playerWeapon.upper() == "SWORD":
-            print("You are now a swordsman.")
+            typingPrint("\033[1;32m SWORD ACQUIRED \033[0;0m")
+            typingPrint(f"""
+        Old Man #2: Wonderful choice! You are now a swordsman
+        Old Man #1: The evil Lord Elron is in his castle in Spike Valley.
+        Old Man #3: We will now teleport you into the Spike Forest, clsoe to the Valley.
+        Old Men in Unison: Good luck {playerName} the Adventurer!
+
+    """)
+            backpack.update({"Weapon" : "Sword"})
+            playerSword()
             break
+
         elif playerWeapon.upper() == "WAND":
-            print("You are now a mage.")
+            typingPrint("\033[1;32m SWORD ACQUIRED \033[0;0m")
+            typingPrint(f"""
+        Old Man #2: Wonderful choice! You are now a swordsman
+        Old Man #1: The evil Lord Elron is in his castle in Spike Valley.
+        Old Man #3: We will now teleport you into the Spike Forest, clsoe to the Valley.
+        Old Men in Unison: Good luck {playerName} the Adventurer!
+
+    """)
+            backpack.update({"Weapon" : "Wand"})
+            playerMage()
             break
-        elif playerWeapon.upper() == "SWORD":
-            print("You are now an archer.")
+
+        elif playerWeapon.upper() == "BOW":
+            typingPrint("\033[1;32m SWORD ACQUIRED \033[0;0m")
+            typingPrint(f"""
+        Old Man #2: Wonderful choice! You are now a swordsman
+        Old Man #1: The evil Lord Elron is on the top of his Spike Tower.
+        Old Man #3: We will now teleport you into the first floor of the tower.
+        Old Men in Unison: Good luck {playerName} the Adventurer!
+
+    """)
+            backpack.update({"Weapon" : "Bow"})
+            playerBow()
             break
         else:
             print("Input not recognized.")
             continue
 
+def playerSword():
+    tower.update({"Tower" : "Entrance"})
+    typingPrint("\n\033[1;32m Player customization is now finished. \033[0;0m\n")
+    typingPrint("\033[1;32m Teleported into Spike Tower (1-F). \033[0;0m\n")
+    typingPrint("\033[1;32m Type B to check backpack. Type L for location. \033[0;0m\n")
+    typingPrint("""
+        
+        As you were summoned in the tower, you see three passages:
+        On the left is a large door.
+        On the middle is a hallway.
+        On the right is a staircase
+
+        Where do you want to go?
+
+                 \033[1mLEFT         MIDDLE        RIGHT\033[0;0m
+
+    """)
+
+    while True:
+        playerChoice = typingInput("\nYou: ")
+        if playerChoice.upper() == "B":
+            typingPrint("\nYour backpack contains: \n")
+            typingPrint(backpack)
+            continue
+        elif playerChoice.upper() == "L":
+            typingPrint("\nYour location is: \n")
+            typingPrint(tower)
+            continue
+        elif playerChoice.upper() == "LEFT":
+            typingPrint("""
+        
+        You entered the big door.
+        There is a skeleton holding a sword.
+        IT'S COMING TOWARDS YOU!
+
+        What do you want to do?
+
+                 \033[1mFIGHT        FLEE\033[0;0m
+
+    """)
+            tower.update({"Tower" : "Big Door"})
+            while True:
+                playerSkeleton = typingInput("You: I will ")
+                if playerSkeleton.upper() == "FIGHT":
+                    typingPrint("\n\033[1;32mYou defeated the skeleton!\033[0;0m\n")
+                    typingPrint("""
+        
+        You searched the room and saw an iron armor.
+        \033[1;32m IRON ARMOR ACQUIRED \033[0;0m
+        You went outside the room and proceeded to the hallway.
+
+    """)
+                    backpack.update({"Armor" : "Iron"})
+                    break
+
+                elif playerSkeleton.upper() == "DEFEND":
+                    typingPrint("\n\033[1;32mThe skeleton caught you.\033[0;0m\n")
+                    typingPrint("\n\033[0;31mYou lost.\033[0;0m\n")
+                    continue
+                else:
+                    print("Input not recognized.")
+                    continue
+
+
+            break
+
+
+
+
+        elif playerChoice.upper() == "MIDDLE":
+            print("On the left")
+            break
+        elif playerChoice.upper() == "RIGHT":
+            print("On the left")
+            break
+        else:
+            print("Input not recognized.")
+            continue
+
+def playerMage():
+    print()
+
+def playerBow():
+    print()
 
 welcome()
 
