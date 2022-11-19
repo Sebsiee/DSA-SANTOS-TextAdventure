0dc14eb28461b0010fd738410ba6e96e0abca2b2
unknown <santosbasty4@gmail.com>
2022-11-19 11:56:01 +0800

    First Commit - Base

---
+++ TextAdventure.py
@@ -0,0 +1,32 @@
+#Adventure Game
+def welcome():
+    print("""..••°°°°••....••°°°°••...••°°°°••....••°°°°••...ELROND'S EMPIRE..••°°°°••....••°°°°••....••°°°°••....••°°°°••..
+                            Welcome to Elrond's Empire! A text-based adventure game.
+ 
+            There are different options you can choose from. These choices will affect your gameplay.
+                    Experience a fantasy world through your imagination! Be an Elrond hero.
+
+..••°°°°••....••°°°°••...••°°°°••....••°°°°••....••°°°°••....••°°°°••....••°°°°••....••°°°°••....••°°°°••..
+    """)
+
+    startGame = input("Are your ready to start your game? Type Y if yes or N if no ")
+    if startGame == "Y":
+        intro()
+
+def intro():
+    print("Welcome")
+    
+
+
+welcome()
+
+    
+#Player Customization
+#Swordsman Option
+#Mage Option
+#Archer Option
+
+
+
+
+
